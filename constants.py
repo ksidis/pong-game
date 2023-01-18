e79bbf305cb734500bbb8b42c1cdb5861857d58d
Kostas Sidiropoulos <ksidis@gmail.com>
2023-01-18 12:00:50 +0000

    initial commit

---
+++ constants.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+FPS = 60
+
+BALL_SIZE = SCREEN_HEIGHT // 50
+BALL_MAX_VELOCITY = 5
+
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = SCREEN_HEIGHT // 8
+PADDLE_VELOCITY = 5
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+@dataclass
+class Point:
+    x: int
+    y: int
